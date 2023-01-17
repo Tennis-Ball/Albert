92e745d243dc3fcf137f75c2d3eb32ab12e1b60e
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2023-01-16 19:25:16 -0500

    parsers for meta and wikiQA

--- data_collection/parsers/meta_convo_parser.py
+++ data_collection/parsers/meta_convo_parser.py
@@ -1,5 +1,23 @@
 # Meta AI Conversational Dataset parser
+import os
+import json
+import re
 
 def parse_meta_convo_ds():
-    pass
+    file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..","datasets/meta_convo_ds/CasualConversations_transcriptions(1).json"))
+    raw = open(file_path, 'r').read()
+    jsonObj = json.loads(raw)
+    transcripts = [data['transcription'] for data in jsonObj]
+
+    #stack overflowed
+    extraTagsRegex = re.compile('[<\[].*?[>\]]')
+
+    #I'm pretty sure first speaker is actually labeled secondary in this dataset
+    firstSpeakerRegex = re.compile('\[secondary_.*?_secondary\]')
+
+    secondSpeakerRegex = re.compile('\[primary.*?_primary\]')
+    return [re.sub(extraTagsRegex,'',re.sub(secondSpeakerRegex, 'Spkr2:', re.sub(firstSpeakerRegex, 'Spkr1:', data))) for data in transcripts]
+
+
+print(parse_meta_convo_ds())
 
--- data_collection/parsers/wiki_QA_parser.py
+++ data_collection/parsers/wiki_QA_parser.py
@@ -1,5 +1,13 @@
 # Wikipedia Question-Answer Corpus parser
+import os
 
-def parse_wiki_QA_ds():
-    pass
+def parse_wiki_QA_ds() -> [str]:
 
+    file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..","datasets/WikiQACorpus/WikiQA-train.txt"))
+    raw = open(file_path, 'r').read()
+    processed = [processLine(l) for l in raw.splitlines()]
+    return processed
+
+def processLine(line:str) -> str:
+    parts = line.split("\t")[:2]
+    return "Spkr1: " + parts[0] + " Spkr2: " + parts[1]
