a132dd49d4ba204f9f06377e0ad77109cbf10fca
Mason Choi <choi.mason@gmail.com>
2023-01-21 21:00:19 -0500

    split data

--- model/main.py
+++ model/main.py
@@ -3,4 +3,4 @@ from process_data import process_data
 import tensorflow as tf
 
 
-xtrain, ytrain, xtest, ytest, xval, yval = process_data(5, (100, 0, 0))
+train, test, val = process_data(5, (100, 0, 0))
--- model/process_data.py
+++ model/process_data.py
@@ -4,17 +4,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 # import tensorflow as tf
 
+
 def get_data() -> [str]:
     file = open("../data_collection/data.json").read()
     return list(filter(lambda x:x != None and len(x) < 2100 and len(x) > 30,json.loads(file)["data"]))
 
+
 # size in percentage of total data, split in form (train%, test%, val%)
 def process_data(size: int=100, split: (int,int,int)=(80, 15, 5)) -> object:
+    data = get_data()  # get data
+    np.random.shuffle(data)
+    data = data[:int(len(data)*(size/100))]  # truncate to specified size
+    data = np.array(data)  # convert to array
+    
+    # calculate data split indices
+    train_i = int(len(data)*(split[0]/100))
+    test_i = int(len(data)*(split[1]/100)) + train_i
+    train, test, val = data[:train_i], data[train_i:test_i], data[test_i:]  # split
+    print(len(train), len(test), len(val))
+
+    return train, test, val
+
+
+def hist():
     data = get_data()
     plt.hist([len(x) for x in data], bins=100)
     plt.show()
     print(len(data))
-    xtrain, ytrain, xtest, ytest, xval, yval = np.array()
-    return xtrain, ytrain, xtest, ytest, xval, yval
 
-process_data()
+process_data(10, (85, 10, 5))
